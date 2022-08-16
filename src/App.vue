f7cf3f2e91a162b738668becfeaca1567c9963b3
ry0y4n <rusk.ryoy4n@gmail.com>
2022-08-16 21:33:27 +0900

    UI更新（テスト用）

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <MomosukeWorld msg="momosuke" />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HOGEHOGE</h1>
   </div>
 </template>
 
