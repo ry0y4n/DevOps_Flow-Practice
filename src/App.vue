c5aad08a338a863840fe8a4489ecd56adf08fd70
ry0y4n <rusk.ryoy4n@gmail.com>
2022-08-17 18:20:01 +0900

    OWASP ZAP用UI更新

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1>Microsoft Intern</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque minus, dolor adipisci eum laudantium saepe. Laborum iusto rerum fugiat dolorum maxime autem repellat iure repudiandae distinctio, libero illo aliquam minus.</p>
   </div>
 </template>
 
